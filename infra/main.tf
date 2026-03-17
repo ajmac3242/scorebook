608b2d47dd9bda7f84c8fb99c7eff1418ea04be7
ajmac3242 <andrew.mccombs@gmail.com>
2026-03-17 15:14:43 -0500

    Remove 'terraform_lock' DynamoDB table resource
    Removed the 'terraform_lock' DynamoDB table resource from the configuration.

--- infra/main.tf
+++ infra/main.tf
@@ -34,17 +34,6 @@ resource "aws_cognito_user_pool_client" "client" {
 }
 
 # --- DynamoDB ---
-resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "basketball-stats-terraform-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
 resource "aws_dynamodb_table" "table" {
   name           = "BasketballStats"
   billing_mode   = "PAY_PER_REQUEST"
