814419e033d09a5e1315aa90c41f7525ad8f5f65
ajmac3242 <andrew.mccombs@gmail.com>
2026-03-18 16:17:42 -0500

    fix(infra): change use_lockfile value to boolean true

--- infra/versions.tf
+++ infra/versions.tf
@@ -6,7 +6,7 @@ terraform {
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    use_lockfile = "basketball-stats-terraform-lock"
+    use_lockfile = true
   }
 
   required_providers {
