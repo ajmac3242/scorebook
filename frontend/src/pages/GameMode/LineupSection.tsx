db25df30c69fa49bfff9fce65760765a3787393d
ajmac3242 <14366384+ajmac3242@users.noreply.github.com>
2026-05-09 17:23:13 +0000

    chore: auto-format and JSDoc stubs [skip ci]

--- frontend/src/pages/GameMode/LineupSection.tsx
+++ frontend/src/pages/GameMode/LineupSection.tsx
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  Box,
-  Typography,
-  Button,
-  Avatar,
-  IconButton,
-} from "@mui/material";
+import { Box, Typography, Button, Avatar, IconButton } from "@mui/material";
 import { History } from "@mui/icons-material";
 import { MoleskineCard } from "../../components/SharedUI";
 import { formatClock, formatPlusMinus } from "../../utils/mathUtils";
@@ -28,7 +22,9 @@ interface LineupSectionProps {
   setSubOutPlayerId: (_id: string | null) => void;
   setIsSubDialogOpen: (_open: boolean) => void;
   chainPrompt: { type: "ASSIST" | "REBOUND"; originalStat: StatEvent } | null;
-  setChainPrompt: (_prompt: { type: "ASSIST" | "REBOUND"; originalStat: StatEvent } | null) => void;
+  setChainPrompt: (
+    _prompt: { type: "ASSIST" | "REBOUND"; originalStat: StatEvent } | null,
+  ) => void;
   handleChainAction: (_playerId: string, _type: string) => void;
 }
 
@@ -140,7 +136,9 @@ export const LineupSection: React.FC<LineupSectionProps> = ({
                 key={emptyId}
                 variant="outlined"
                 disabled={isReadOnly}
-                aria-label={"Empty lineup slot " + (i + 1) + ", click to assign player"}
+                aria-label={
+                  "Empty lineup slot " + (i + 1) + ", click to assign player"
+                }
                 onClick={() => {
                   setSubOutPlayerId(emptyId);
                   setIsSubDialogOpen(true);
