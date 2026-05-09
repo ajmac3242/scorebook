93f68d7c7a462b1d44149aa79561fa65aa6ab8a7
ajmac3242 <14366384+ajmac3242@users.noreply.github.com>
2026-05-09 14:09:53 +0000

    chore: auto-format and JSDoc stubs [skip ci]

--- frontend/src/pages/GameMode.tsx
+++ frontend/src/pages/GameMode.tsx
@@ -42,9 +42,7 @@ import {
   FlashOn,
   ArrowBack,
 } from "@mui/icons-material";
-import {
-  useMediaQuery,
-} from "@mui/material";
+import { useMediaQuery } from "@mui/material";
 import BasketballCourt from "../components/BasketballCourt";
 import RecentActionItem from "../components/RecentActionItem";
 import QuickSubDialog from "../components/QuickSubDialog";
