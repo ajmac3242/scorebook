95a90b04add4edc22dd06728b3633331ffcf8fc0
ajmac3242 <andrew.mccombs@gmail.com>
2026-06-09 08:51:10 -0500

    refactor(player-stats): trim exports for new stats layout

--- frontend/src/pages/PlayerStats/index.ts
+++ frontend/src/pages/PlayerStats/index.ts
@@ -1,7 +1,5 @@
-export * from "./hooks/usePlayerStatsData";
-export * from "./hooks/usePlayerStatsFilters";
-export { default as PlayerSummaryCard } from "./sections/PlayerSummaryCard";
-export { default as PlayerShotChartCard } from "./sections/PlayerShotChartCard";
-export { default as PlayerGameLogCard } from "./sections/PlayerGameLogCard";
+export { usePlayerStatsData } from "./hooks/usePlayerStatsData";
+export { usePlayerStatsFilters } from "./hooks/usePlayerStatsFilters";
 export { default as PlayerStatsFilterBar } from "./sections/PlayerStatsFilterBar";
+export { default as PlayerShotChartCard } from "./sections/PlayerShotChartCard";
 export { default as EditPlayerDialog } from "./dialogs/EditPlayerDialog";
