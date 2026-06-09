f1b712f0b6cd7378abae2ee6884ad34a7c97cb0b
ajmac3242 <andrew.mccombs@gmail.com>
2026-06-09 06:21:51 -0500

    chore(players): update PlayerStats index — export PlayerGameLogCard, remove PlayerActionLogCard

--- frontend/src/pages/PlayerStats/index.ts
+++ frontend/src/pages/PlayerStats/index.ts
@@ -1,7 +1,7 @@
 export * from "./hooks/usePlayerStatsData";
 export * from "./hooks/usePlayerStatsFilters";
-export { default as PlayerSummaryCard } from "./sections/PlayerSummaryCard";
-export { default as PlayerShotChartCard } from "./sections/PlayerShotChartCard";
-export { default as PlayerActionLogCard } from "./sections/PlayerActionLogCard";
+export { default as PlayerSummaryCard }    from "./sections/PlayerSummaryCard";
+export { default as PlayerShotChartCard }  from "./sections/PlayerShotChartCard";
+export { default as PlayerGameLogCard }    from "./sections/PlayerGameLogCard";
 export { default as PlayerStatsFilterBar } from "./sections/PlayerStatsFilterBar";
-export { default as EditPlayerDialog } from "./dialogs/EditPlayerDialog";
+export { default as EditPlayerDialog }     from "./dialogs/EditPlayerDialog";
