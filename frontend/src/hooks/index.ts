e792a0301b8d5a7336ac241756010086f7cf2ca3
ajmac3242 <andrew.mccombs@gmail.com>
2026-06-09 07:02:22 -0500

    chore(hooks): export useRosterAggregates and RosterPlayerStats from index

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -6,8 +6,10 @@ export { useStatWriter } from "./useStatWriter";
 export { useGameAggregator } from "./useGameAggregator";
 export { useVoiceRecognition } from "./useVoiceRecognition";
 export { usePageSnackbar } from "./usePageSnackbar";
+export { useRosterAggregates } from "./useRosterAggregates";
 export type {
   SnackbarSeverity,
   SnackbarState,
   UsePageSnackbarReturn,
 } from "./usePageSnackbar";
+export type { RosterPlayerStats } from "./useRosterAggregates";
